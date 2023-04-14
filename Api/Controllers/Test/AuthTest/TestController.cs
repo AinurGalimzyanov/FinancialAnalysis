cd709a0f4d2b2587c98ac3cdec9b537fd4b8c51c
Galimzyanov Ainur <a.galimz@mail.ru>
2023-04-14 19:38:58 +0500

    тест

---
+++ Api/Controllers/Test/AuthTest/TestController.cs
@@ -0,0 +1,28 @@
+﻿using Api.Controllers.Public.Auth.Dto.Request;
+using Api.Controllers.Public.Base;
+using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers.Public.Auth;
+
+
+
+public class TestController : BasePublicController
+{
+    private readonly IMapper _mapper;
+
+    public TestController(IMapper mapper)
+    {
+        _mapper = mapper;
+    }
+
+    [HttpPost("test")]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public async Task<IActionResult> Test([FromBody] SignInModelRequest modelRequest)
+    {
+        return Ok();
+    }
+}
